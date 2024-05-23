6163ced39bee190c3a6365a9bd8b8989226dac48
G0m1bak0 <lyhenry2004@gmail.com>
2024-05-23 16:21:18 +1200

    Implemented an early findShortestRoute but need to edit

--- src/main/java/nz/ac/auckland/se281/WorldMap.java
+++ src/main/java/nz/ac/auckland/se281/WorldMap.java
@@ -1,9 +1,13 @@
 package nz.ac.auckland.se281;
 
 import java.util.Map;
+import java.util.Queue;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
 
 public class WorldMap {
   private Map<Country, List<Country>> adjCountries;
@@ -32,4 +36,37 @@ public class WorldMap {
     
     return null;
   }
+
+  public List<Country> findRoute(Country start, Country end) {
+    Set<Country> visited = new HashSet<>();
+    Map<Country, Country> neighbourMap = new HashMap<>();
+    Queue<Country> queue = new LinkedList<>();  
+
+    queue.add(start);
+    visited.add(start);
+    neighbourMap.put(start, null);
+
+    while (!queue.isEmpty()) {
+        Country currentCountry = queue.poll();
+        for (Country neighbour : adjCountries.get(currentCountry)) {
+            if (neighbour.equals(end)) {
+                // Destination detected
+                List<Country> shortestRoute = new LinkedList<>();
+                // Add destination to route
+                shortestRoute.add(neighbour);
+                Country nextCountry = currentCountry;
+                // Loop until start is reached again
+                while (nextCountry != null) {
+                  shortestRoute.add(nextCountry);
+                  nextCountry = neighbourMap.get(nextCountry);
+                }
+            } else if (!visited.contains(neighbour)) {
+              visited.add(neighbour);
+              queue.add(neighbour);
+              neighbourMap.put(neighbour, currentCountry);
+            }
+        }
+    }
+    return null;
+ }
 }
